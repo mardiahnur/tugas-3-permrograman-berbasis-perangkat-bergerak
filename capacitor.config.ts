4f7cede7f92e03832a3425820fbbb1584c613ba9
mardiahnur <rahmimardiah92@gmail.com>
2025-11-28 21:40:42 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugas-3',
+  webDir: 'dist'
+};
+
+export default config;
